969a53d5267aad5777d38f68dfeffec222bf59fc
St1ch <st1ch@mail.ru>
2017-08-02 21:54:39 +0300

    add header, slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
-	// Custom JS
+	$('.owl-carousel').owlCarousel({
+		Default: 1,
+    loop:true,
+    nav:true,
+    autoHeight:true,
+    mouseDrag: false,
+    touchDrag: false,
+    margin: 0,
+    dots: true,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+	})
 
 });
